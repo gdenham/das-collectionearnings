6f498fc6c9dd54d94e6553fb69ce31445ed738d4
Costin Grigorescu <costin.grigorescu@civil.lmco.com>
2016-08-12 15:21:16 +0100

    Add unit tests for the DC context wrapper class.

---
+++ src/SFA.DAS.CollectionEarnings.Infrastructure.UnitTests/DcContext/DcContextWrapperTests/WhenGettingPropertyValue.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using CS.Common.External.Interfaces;
+using NUnit.Framework;
+using SFA.DAS.CollectionEarnings.Infrastructure.DcContext;
+
+namespace SFA.DAS.CollectionEarnings.Infrastructure.UnitTests.DcContext.DcContextWrapperTests
+{
+    public class WhenGettingPropertyValue
+    {
+        private DcContextWrapper _contextWrapper;
+
+        [SetUp]
+        public void Arrange()
+        {
+            var context = new ExternalContext();
+            context.Properties = new Dictionary<string, string>
+                {
+                    { "key", "value" }
+                };
+
+            _contextWrapper = new DcContextWrapper(context);
+        }
+
+        [Test]
+        public void ThenCorrectValueForKnownProperty()
+        {
+            // Act
+            var val = _contextWrapper.GetPropertyValue("key");
+
+            // Assert
+            Assert.AreEqual("value", val);
+        }
+
+        [Test]
+        public void ThenDefaultValueForUnknownPropertyWithDefaultValue()
+        {
+            // Act
+            var defaultVal = "defaultvalue";
+            var val = _contextWrapper.GetPropertyValue("unknownkey", defaultVal);
+
+            // Assert
+            Assert.AreEqual(defaultVal, val);
+        }
+
+        [Test]
+        public void ThenNullForUnknownPropertyWithNoDefaultValue()
+        {
+            // Act
+            var val = _contextWrapper.GetPropertyValue("unknownkey");
+
+            // Assert
+            Assert.IsNull(val);
+        }
+
+        internal class ExternalContext : IExternalContext
+        {
+            public IDictionary<string, string> Properties { get; set; }
+        }
+    }
+}
