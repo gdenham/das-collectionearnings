169e06596c7e44767c0962e1a46fd2ae2ed644b5
Costin Grigorescu <costin.grigorescu@civil.lmco.com>
2016-08-18 12:01:53 +0100

    Add initial ValidationError entity and ValidationErrorRepository implementation for adding a data lock validation error in the ILR transient database.

---
+++ src/datalock/SFA.DAS.CollectionEarnings.DataLock/Data/Entities/ValidationError.cs
@@ -0,0 +1,12 @@
+﻿using Dapper.Contrib.Extensions;
+
+namespace SFA.DAS.CollectionEarnings.DataLock.Data.Entities
+{
+    [Table("DataLock.ValidationError")]
+    public class ValidationError
+    {
+        public string LearnRefNumber { get; set; }
+        public long AimSeqNumber { get; set; }
+        public string RuleId { get; set; }
+    }
+}
---
+++ src/datalock/SFA.DAS.CollectionEarnings.DataLock/Data/Repositories/IValidationErrorRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using SFA.DAS.CollectionEarnings.DataLock.Data.Entities;
+
+namespace SFA.DAS.CollectionEarnings.DataLock.Data.Repositories
+{
+    public interface IValidationErrorRepository
+    {
+        void AddValidationError(ValidationError validationError);
+
+        void AddValidationErrors(List<ValidationError> validationErrors);
+    }
+}
