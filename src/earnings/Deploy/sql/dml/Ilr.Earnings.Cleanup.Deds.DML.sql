f13f6a18ef698e4ac86380d67e6215e6982aeb08
Costin Grigorescu <costin.grigorescu@civil.lmco.com>
2016-10-17 15:03:24 +0100

    Adjust ILR_Deds token.

--- src/earnings/Deploy/sql/dml/Ilr.Earnings.Cleanup.Deds.DML.sql
+++ src/earnings/Deploy/sql/dml/Ilr.Earnings.Cleanup.Deds.DML.sql
@@ -1,7 +1,7 @@
-DELETE FROM ${ILR_Deds}.Rulebase.AE_LearningDelivery
+DELETE FROM ${ILR_Deds.FQ}.Rulebase.AE_LearningDelivery
     WHERE Ukprn IN (SELECT Ukprn FROM [Valid].[LearningProvider])
 GO
 
-DELETE FROM ${ILR_Deds}.Rulebase.AE_LearningDelivery_PeriodisedValues
+DELETE FROM ${ILR_Deds.FQ}.Rulebase.AE_LearningDelivery_PeriodisedValues
     WHERE Ukprn IN (SELECT Ukprn FROM [Valid].[LearningProvider])
 GO
