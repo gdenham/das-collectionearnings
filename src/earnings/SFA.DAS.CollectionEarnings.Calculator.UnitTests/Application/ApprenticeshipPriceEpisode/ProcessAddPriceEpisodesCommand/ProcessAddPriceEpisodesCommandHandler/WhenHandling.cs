7430a9b54004b889e075dd2321c5c44f0f548a7b
Costin Grigorescu <costin.grigorescu@civil.lmco.com>
2016-12-16 14:52:53 +0000

    Improved unit test on adding price episodes.

--- src/earnings/SFA.DAS.CollectionEarnings.Calculator.UnitTests/Application/ApprenticeshipPriceEpisode/ProcessAddPriceEpisodesCommand/ProcessAddPriceEpisodesCommandHandler/WhenHandling.cs
+++ src/earnings/SFA.DAS.CollectionEarnings.Calculator.UnitTests/Application/ApprenticeshipPriceEpisode/ProcessAddPriceEpisodesCommand/ProcessAddPriceEpisodesCommandHandler/WhenHandling.cs
@@ -3,13 +3,22 @@ using Moq;
 using NUnit.Framework;
 using SFA.DAS.CollectionEarnings.Calculator.Application.ApprenticeshipPriceEpisode.ProcessAddPriceEpisodesCommand;
 using SFA.DAS.CollectionEarnings.Calculator.Infrastructure.Data;
+using SFA.DAS.CollectionEarnings.Calculator.Infrastructure.Data.Entities;
 using SFA.DAS.CollectionEarnings.Calculator.UnitTests.Tools.Builders;
 
 namespace SFA.DAS.CollectionEarnings.Calculator.UnitTests.Application.ApprenticeshipPriceEpisode.ProcessAddPriceEpisodesCommand.ProcessAddPriceEpisodesCommandHandler
 {
     public class WhenHandling
     {
-        // TODO: check that the domain object has the same values as the entity sent to the repository
+        private static readonly Calculator.Application.ApprenticeshipPriceEpisode.ApprenticeshipPriceEpisode[] PriceEpisodes = 
+        {
+            new ApprenticeshipPriceEpisodeBuilder().Build(),
+            new ApprenticeshipPriceEpisodeBuilder().WithAimSeqNumber(2).Build(),
+            new ApprenticeshipPriceEpisodeBuilder().WithAimSeqNumber(3).Build(),
+            new ApprenticeshipPriceEpisodeBuilder().WithAimSeqNumber(4).Build(),
+            new ApprenticeshipPriceEpisodeBuilder().WithAimSeqNumber(5).Build()
+        };
+
         private Mock<IApprenticeshipPriceEpisodeRepository> _repository;
 
         private ProcessAddPriceEpisodesCommandRequest _request;
@@ -22,14 +31,7 @@ namespace SFA.DAS.CollectionEarnings.Calculator.UnitTests.Application.Apprentice
 
             _request = new ProcessAddPriceEpisodesCommandRequest
             {
-                PriceEpisodes = new []
-                {
-                    new ApprenticeshipPriceEpisodeBuilder().Build(),
-                    new ApprenticeshipPriceEpisodeBuilder().WithAimSeqNumber(2).Build(),
-                    new ApprenticeshipPriceEpisodeBuilder().WithAimSeqNumber(3).Build(),
-                    new ApprenticeshipPriceEpisodeBuilder().WithAimSeqNumber(4).Build(),
-                    new ApprenticeshipPriceEpisodeBuilder().WithAimSeqNumber(5).Build()
-                }
+                PriceEpisodes = PriceEpisodes
             };
 
             _handler = new Calculator.Application.ApprenticeshipPriceEpisode.ProcessAddPriceEpisodesCommand.ProcessAddPriceEpisodesCommandHandler(_repository.Object);
@@ -38,10 +40,6 @@ namespace SFA.DAS.CollectionEarnings.Calculator.UnitTests.Application.Apprentice
         [Test]
         public void ThenValidResponseReturnedForValidRepositoryResponse()
         {
-            // Arrange
-            _repository
-                .Setup(r => r.AddApprenticeshipPriceEpisodes(It.IsAny<Infrastructure.Data.Entities.ApprenticeshipPriceEpisodeEntity[]>()));
-
             // Act
             var response = _handler.Handle(_request);
 
@@ -50,15 +48,60 @@ namespace SFA.DAS.CollectionEarnings.Calculator.UnitTests.Application.Apprentice
         }
 
         [Test]
+        public void ThenItShouldWriteThePriceEpisodesToTheRepository()
+        {
+            // Act
+            _handler.Handle(_request);
+
+            // Assert
+            _repository.Verify(r => r.AddApprenticeshipPriceEpisodes(It.Is<ApprenticeshipPriceEpisodeEntity[]>(l => PriceEpisodesBatchesMatch(l, PriceEpisodes))));
+        }
+
+        [Test]
         public void ThenInvalidResponseReturnedForInvalidRepositoryResponse()
         {
             // Arrange
             _repository
-                .Setup(r => r.AddApprenticeshipPriceEpisodes(It.IsAny<Infrastructure.Data.Entities.ApprenticeshipPriceEpisodeEntity[]>()))
+                .Setup(r => r.AddApprenticeshipPriceEpisodes(It.IsAny<ApprenticeshipPriceEpisodeEntity[]>()))
                 .Throws<Exception>();
 
             // Assert
             Assert.Throws<Exception>(() => _handler.Handle(_request));
+        }
+
+        private bool PriceEpisodesBatchesMatch(ApprenticeshipPriceEpisodeEntity[] entities, Calculator.Application.ApprenticeshipPriceEpisode.ApprenticeshipPriceEpisode[] priceEpisodes)
+        {
+            if (entities.Length != priceEpisodes.Length)
+            {
+                return false;
+            }
+
+            for (var x = 0; x < entities.Length; x++)
+            {
+                if (!PriceEpisodesMatch(entities[x], priceEpisodes[x]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool PriceEpisodesMatch(ApprenticeshipPriceEpisodeEntity entity, Calculator.Application.ApprenticeshipPriceEpisode.ApprenticeshipPriceEpisode priceEpisode)
+        {
+            return entity.PriceEpisodeIdentifier == priceEpisode.Id
+                && entity.LearnRefNumber == priceEpisode.LearnerReferenceNumber
+                && entity.EpisodeEffectiveTNPStartDate == priceEpisode.StartDate
+                && entity.EpisodeStartDate == priceEpisode.StartDate
+                && entity.PriceEpisodeActualEndDate == priceEpisode.ActualEndDate
+                && entity.PriceEpisodeAimSeqNumber == priceEpisode.AimSequenceNumber
+                && entity.PriceEpisodeCompletionElement == priceEpisode.CompletionAmount
+                && entity.PriceEpisodeInstalmentValue == priceEpisode.MonthlyAmount
+                && entity.PriceEpisodeTotalTNPPrice == priceEpisode.NegotiatedPrice
+                && entity.TNP1 == priceEpisode.Tnp1
+                && entity.TNP2 == priceEpisode.Tnp2
+                && entity.TNP3 == priceEpisode.Tnp3
+                && entity.TNP4 == priceEpisode.Tnp4;
         }
     }
 }
